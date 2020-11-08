a7f516566f8a364fa570a86e13616df3de8af785
Dan Ngo <dan.ngo.tech@gmail.com>
2020-11-08 16:28:09 -0500

    greaterOrEqual and lessOrEqual decorators

---
+++ src/validators/greaterOrEqual.ts
@@ -0,0 +1,28 @@
+import { paramValidateBuilder } from "../core/paramValidateBuilder";
+import { greaterOrEqualValidator } from "../core/paramValidators";
+import { DecoratorMetadata } from "../types/DecoratorMetadata";
+
+/**
+ * NOTE: return value of the decorator function 
+ * is ignored. With this in mind, how in the world can
+ * we create parameter decorator factories?
+ * 
+ * Decorator arguments:
+ * target: Either the constructor function of the class 
+ * for a static member, or the prototype of the class 
+ * for an instance member.
+ * 
+ * propertyKey: The name of the member.
+ * 
+ * parameterIndex: The ordinal index of the parameter 
+ * in the function’s parameter list.
+ */
+export function greaterOrEqual(threshold: number) {
+  return function (...config: DecoratorMetadata) {
+    paramValidateBuilder(
+      "greaterOrEqual",
+      greaterOrEqualValidator(threshold),
+      config,
+    );
+  }
+}
